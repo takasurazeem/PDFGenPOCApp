5cfa74080acdf6abc90b92a9ed75338b40b5f525
Takasur Azeem <takasurazeem@gmail.com>
2024-11-10 23:59:17 +0500

    code clean up

--- PDFGenPOCApp/MainPage.xaml.cs
+++ PDFGenPOCApp/MainPage.xaml.cs
@@ -1,157 +1,147 @@
-﻿using DrawnUi.Maui.Draw;
-using DrawnUi.Maui.Infrastructure;
+﻿using DrawnUi.Maui.Infrastructure;
 using SkiaSharp;
 
-namespace PDFGenPOCApp
+namespace PDFGenPOCApp;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private string _BindableText;
+    private bool _lockLogs;
+
+    public MainPage()
     {
-
-        public MainPage()
+        try
         {
-            try
-            {
-                InitializeComponent();
-            }
-            catch (Exception e)
-            {
-                Super.DisplayException(this, e);
-            }
+            InitializeComponent();
         }
-        string GenerateFileName(DateTime timeStamp, string extension)
+        catch (Exception e)
         {
-            var filename = $"DrawnUi_{timeStamp:yyyy-MM-dd_HHmmss}.{extension}";
-
-            return filename;
+            Super.DisplayException(this, e);
         }
-        private bool _lockLogs;
+    }
 
-        private string _BindableText;
-        public string BindableText
+    public string BindableText
+    {
+        get => _BindableText;
+        set
         {
-            get
+            if (_BindableText != value)
             {
-                return _BindableText;
-            }
-            set
-            {
-                if (_BindableText != value)
-                {
-                    _BindableText = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        async Task CreatePdf(float width)
-        {
-            //setup our report to print
-            BindableText = "This text came from bindings";
-            var vendor = "DrawnUI";
-            var filename = GenerateFileName(DateTime.Now, "pdf");
-
-            var layout = new ReportSample()
-            {
-                BindingContext = this //whatever you want, you can have bindings inside your report
-            };
-
-            //render and share
-            Files.CheckPermissionsAsync(async () =>
-             {
-
-                 try
-                 {
-                     _lockLogs = true;
-                     string fullFilename = null;
-                     var subfolder = "Pdf";
-                     var scale = 1; //do not change this
-                     var destination = new SKRect(0, 0, width, float.PositiveInfinity);
-                     var measured = layout.Measure(destination.Width, destination.Height, scale);
-
-                     //prepare DrawingRect
-                     layout.Arrange(new SKRect(0, 0, layout.MeasuredSize.Pixels.Width, layout.MeasuredSize.Pixels.Height),
-                         layout.MeasuredSize.Pixels.Width, layout.MeasuredSize.Pixels.Height, scale);
-
-                     var reportSize = new SKSize(measured.Units.Width, measured.Units.Height);
-
-                     //we need a local file to ba saved in order to share it
-                     fullFilename = Files.GetFullFilename(filename, StorageType.Cache, subfolder);
-
-                     if (File.Exists(fullFilename))
-                     {
-                         File.Delete(fullFilename);
-                     }
-
-                     var area = new SKRect(layout.DrawingRect.Left, layout.DrawingRect.Top, reportSize.Width, reportSize.Height);
-
-                     using (var ms = new MemoryStream())
-                     using (var stream = new SKManagedWStream(ms))
-                     {
-                         using (var document = SKDocument.CreatePdf(stream, new SKDocumentPdfMetadata
-                         {
-                             Author = vendor,
-                             Producer = vendor,
-                             Subject = this.Title
-                         }))
-                         {
-                             using (var canvas = document.BeginPage(reportSize.Width, reportSize.Height))
-                             {
-                                 var ctx = new SkiaDrawingContext()
-                                 {
-                                     Canvas = canvas,
-                                     Width = reportSize.Width,
-                                     Height = reportSize.Height
-                                 };
-
-                                 //with no async stuff this is enough for most cases
-                                 layout.Render(ctx, new SKRect(0, 0, reportSize.Width, reportSize.Height), scale);
-                             }
-                             document.EndPage();
-                             document.Close();
-                         }
-
-                         ms.Position = 0;
-                         var content = ms.ToArray();
-
-                         var file = Files.OpenFile(fullFilename, StorageType.Cache, subfolder);
-
-                         // Write the bytes to the FileStream of the FileDescriptor
-                         await file.Handler.WriteAsync(content, 0, content.Length);
-
-                         // Ensure all bytes are written to the underlying device
-                         await file.Handler.FlushAsync();
-
-                         Files.CloseFile(file, true);
-                         await Task.Delay(500);
-                     }
-
-                     //can share the file now
-                     Files.Share("PDF", new string[] { fullFilename });
-                 }
-                 catch (Exception e)
-                 {
-                     Super.Log(e);
-                 }
-                 finally
-                 {
-                     _lockLogs = false;
-                 }
-             });
-        }
-
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            var status = await Permissions.RequestAsync<Permissions.StorageWrite>();
-
-            if (status == PermissionStatus.Granted)
-            {
-                CreatePdf(1240);  // A4 page for 150 DPI
-            }
-            else
-            {
-                // Handle the case where permission is denied
-                Console.WriteLine("Permission denied to access storage.");
+                _BindableText = value;
+                OnPropertyChanged();
             }
         }
     }
 
+    private string GenerateFileName(DateTime timeStamp, string extension)
+    {
+        var filename = $"DrawnUi_{timeStamp:yyyy-MM-dd_HHmmss}.{extension}";
+
+        return filename;
+    }
+
+    private async Task CreatePdf(float width)
+    {
+        //setup our report to print
+        BindableText = "This text came from bindings";
+        var vendor = "DrawnUI";
+        var filename = GenerateFileName(DateTime.Now, "pdf");
+
+        var layout = new ReportSample
+        {
+            BindingContext = this //whatever you want, you can have bindings inside your report
+        };
+
+        //render and share
+        Files.CheckPermissionsAsync(async () =>
+        {
+            try
+            {
+                _lockLogs = true;
+                string fullFilename = null;
+                var subfolder = "Pdf";
+                var scale = 1; //do not change this
+                var destination = new SKRect(0, 0, width, float.PositiveInfinity);
+                var measured = layout.Measure(destination.Width, destination.Height, scale);
+
+                //prepare DrawingRect
+                layout.Arrange(new SKRect(0, 0, layout.MeasuredSize.Pixels.Width, layout.MeasuredSize.Pixels.Height),
+                    layout.MeasuredSize.Pixels.Width, layout.MeasuredSize.Pixels.Height, scale);
+
+                var reportSize = new SKSize(measured.Units.Width, measured.Units.Height);
+
+                //we need a local file to ba saved in order to share it
+                fullFilename = Files.GetFullFilename(filename, StorageType.Cache, subfolder);
+
+                if (File.Exists(fullFilename)) File.Delete(fullFilename);
+
+                var area = new SKRect(layout.DrawingRect.Left, layout.DrawingRect.Top, reportSize.Width,
+                    reportSize.Height);
+
+                using (var ms = new MemoryStream())
+                using (var stream = new SKManagedWStream(ms))
+                {
+                    using (var document = SKDocument.CreatePdf(stream, new SKDocumentPdfMetadata
+                           {
+                               Author = vendor,
+                               Producer = vendor,
+                               Subject = Title
+                           }))
+                    {
+                        using (var canvas = document.BeginPage(reportSize.Width, reportSize.Height))
+                        {
+                            var ctx = new SkiaDrawingContext
+                            {
+                                Canvas = canvas,
+                                Width = reportSize.Width,
+                                Height = reportSize.Height
+                            };
+
+                            //with no async stuff this is enough for most cases
+                            layout.Render(ctx, new SKRect(0, 0, reportSize.Width, reportSize.Height), scale);
+                        }
+
+                        document.EndPage();
+                        document.Close();
+                    }
+
+                    ms.Position = 0;
+                    var content = ms.ToArray();
+
+                    var file = Files.OpenFile(fullFilename, StorageType.Cache, subfolder);
+
+                    // Write the bytes to the FileStream of the FileDescriptor
+                    await file.Handler.WriteAsync(content, 0, content.Length);
+
+                    // Ensure all bytes are written to the underlying device
+                    await file.Handler.FlushAsync();
+
+                    Files.CloseFile(file, true);
+                    await Task.Delay(500);
+                }
+
+                //can share the file now
+                Files.Share("PDF", new[] { fullFilename });
+            }
+            catch (Exception e)
+            {
+                Super.Log(e);
+            }
+            finally
+            {
+                _lockLogs = false;
+            }
+        });
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var status = await Permissions.RequestAsync<Permissions.StorageWrite>();
+
+        if (status == PermissionStatus.Granted)
+            CreatePdf(1240); // A4 page for 150 DPI
+        else
+            // Handle the case where permission is denied
+            Console.WriteLine("Permission denied to access storage.");
+    }
 }
